1d3b7160c073453ae3bb96a79e8e92b4dd33d8c2
gevin <flyhigher139@gmail.com>
2022-05-31 21:21:36 +0800

    cglib

--- src/main/java/com/igevin/trywithlock/proxy/CglibLockProxy.java
+++ src/main/java/com/igevin/trywithlock/proxy/CglibLockProxy.java
@@ -1,4 +1,48 @@
 package com.igevin.trywithlock.proxy;
 
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+
+import java.util.concurrent.locks.Lock;
+
 public class CglibLockProxy {
+    private static <T> T createLockProxyObject(Class<T> objectClass, MethodInterceptor interceptor) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(objectClass);
+        enhancer.setCallback(interceptor);
+        return (T) enhancer.create();
+    }
+
+    public static <T> T createAutoLockObject(Class<T> objectClass, Lock lock) {
+        MethodInterceptor interceptor = createLockInterceptor(lock);
+        return createLockProxyObject(objectClass, interceptor);
+    }
+
+    public static <T> T createAInterruptibleLockObject(Class<T> objectClass, Lock lock) {
+        MethodInterceptor interceptor = createInterruptibleLockInterceptor(lock);
+        return createLockProxyObject(objectClass, interceptor);
+    }
+
+    private static MethodInterceptor createLockInterceptor(Lock lock) {
+        return (obj, method, args, proxy) -> {
+            try {
+                lock.lock();
+                return proxy.invoke(obj, args);
+            } finally {
+                lock.unlock();
+            }
+        };
+    }
+
+    private static MethodInterceptor createInterruptibleLockInterceptor(Lock lock) {
+        return (obj, method, args, proxy) -> {
+            try {
+                lock.lockInterruptibly();
+                return proxy.invoke(obj, args);
+            } finally {
+                lock.unlock();
+            }
+        };
+    }
+
 }
